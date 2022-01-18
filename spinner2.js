f867fc625926bba3f0537b65634d529404b6a94d
Matthew <m.bednarski@gmail.com>
2022-01-18 23:18:05 +0000

    added \n for qol

--- spinner2.js
+++ spinner2.js
@@ -1,5 +1,5 @@
 let timer = 100;
-const spinner = ['|','/','-','\\','|','/','-','\\','|'];
+const spinner = ['|','/','-','\\','|','/','-','\\','|','\n'];
 for (let line of spinner) {
   setTimeout(() => {process.stdout.write('\r'+ line)}, timer);
   timer += 200;
